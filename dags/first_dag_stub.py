2a895bfe05b572689450eccb6f0b3fff9789c615
Matthieu Roux <36091631+M4TTRX@users.noreply.github.com>
2022-01-21 00:40:37 +0100

    Implemented most generators and attribute saving

--- dags/first_dag_stub.py
+++ dags/first_dag_stub.py
@@ -1,5 +1,6 @@
 # various imports
 from urllib import response
+from warnings import catch_warnings
 import airflow
 import datetime
 from airflow import DAG
@@ -7,29 +8,36 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 import urllib.request as request
-from faker import Faker # for creating fake names
-import logging # for logging in airflow
-import json # for json parsing
-from random import randint # for getting random integers, choosing a random item in a response
+from faker import Faker  # for creating fake names
+import logging  # for logging in airflow
+import json  # for json parsing
+from random import (
+    randint,
+)  # for getting random integers, choosing a random item in a response
 
 
 default_args_dict = {
-    'start_date': airflow.utils.dates.days_ago(0),
-    'concurrency': 1,
-    'schedule_interval': None, # no schedule
-    'retries': 1,   # number of retries
-    'retry_delay': datetime.timedelta(minutes=5),   # delay between retries
+    "start_date": airflow.utils.dates.days_ago(0),
+    "concurrency": 1,
+    "schedule_interval": None,  # no schedule
+    "retries": 1,  # number of retries
+    "retry_delay": datetime.timedelta(minutes=5),  # delay between retries
 }
 
 first_dag = DAG(
-    dag_id='first_dag_stub',
+    dag_id="first_dag_stub",
     default_args=default_args_dict,
-    # catchup: in case of failure you can pause the DAG, when restarting the DAG it will 
-    #   resume from the point when it left. But be careful, if you pause your dag and restart it 
-    #   After a long time the diff of time between dags will cause something that i didn't understand but 
+    # catchup: in case of failure you can pause the DAG, when restarting the DAG it will
+    #   resume from the point when it left. But be careful, if you pause your dag and restart it
+    #   After a long time the diff of time between dags will cause something that i didn't understand but
     #   its bad so thats why we put it to False.
-    catchup=False,  
+    catchup=False,
 )
+
+NUM_CHARACTERS = 5
+MAX_LEVEL = 3
+
+
 def get(url):
     # create the http request
     req = request.Request(url)
@@ -37,88 +45,195 @@ def get(url):
 
     # decode data to json
     raw_data = r.read()
-    encoding = r.info().get_content_charset('utf8')  # JSON default
-    
+    encoding = r.info().get_content_charset("utf8")  # JSON default
+
     # parse json in python dict
     return json.loads(raw_data.decode(encoding))
 
+
+def save_json(data, file_name):
+    with open(f"dags/temp/{file_name}.json", "w") as outfile:
+        json.dump(data, outfile)
+
+
+def read_json(file_name):
+    with open("dags/temp/names.json") as json_file:
+        return json.load(json_file)
+
+
 def _generate_name():
     fake = Faker()
-    name = fake.name()
-    logging.info(f'Created name: {name}')
+    names = [fake.name() for _ in range(NUM_CHARACTERS)]
+    try:
+        save_json(names, "names")
+        logging.info(f"Created names: {names}")
+    except Exception:
+        logging.info(f"Error saving names: {Exception.args}")
+
+
+def _generate_level():
+    levels = [randint(1, MAX_LEVEL) for _ in range(NUM_CHARACTERS)]
+    try:
+        save_json(levels, "levels")
+        logging.info(f"Created levels: {levels}")
+    except Exception:
+        logging.info(f"Error saving levels: {Exception.args}")
+
+
+def _generate_attribute():
+    attributes = [
+        {
+            "strength": randint(6, 18),
+            "dexterity": randint(2, 18),
+            "constitution": randint(2, 18),
+            "intelligence": randint(2, 18),
+            "wisdom": randint(2, 18),
+            "charisma": randint(2, 18),
+        }
+        for _ in range(NUM_CHARACTERS)
+    ]
+    try:
+        save_json(attributes, "attributes")
+        logging.info(f"Created attributes: {attributes}")
+    except Exception:
+        logging.info(f"Error saving attributes: {Exception.args}")
+
 
 def _generate_class():
     # get the list of classes from the api
-    url = 'https://www.dnd5eapi.co/api/classes'
-    response = get(url)
-    # choose a random class from there
-    character_class = response["results"][randint(0, response["count"])]["index"]
-    logging.info(f'Created class: {character_class}')
+    try:
+        url = "https://www.dnd5eapi.co/api/classes"
+        response = get(url)
+        # choose a random class from there
+    except Exception:
+        logging.info(f"Error retrieving classes from api: {Exception.args}")
+        return
+    character_classes = [
+        response["results"][randint(0, response["count"] - 1)]["index"]
+        for _ in range(NUM_CHARACTERS)
+    ]
+    logging.info(f"Created class: {character_classes}")
+    try:
+        save_json(character_classes, "classes")
+    except Exception:
+        logging.info(f"Error saving classes: {Exception.args}")
+
+
+def _generate_language():
+    # get the list of languages from the api
+    try:
+        url = "https://www.dnd5eapi.co/api/languages"
+        response = get(url)
+        # choose a random language
+        character_languages = [
+            response["results"][randint(0, response["count"] - 1)]["index"]
+            for _ in range(NUM_CHARACTERS)
+        ]
+        logging.info(f"Created language: {character_languages}")
+    except Exception:
+        logging.info(f"Error retrieving languages: {Exception}")
+    try:
+        save_json(character_languages, "languages")
+    except Exception:
+        logging.info(f"Error saving languages: {Exception}")
+
+
+def _generate_race():
+    # get the list of races from the api
+    try:
+        url = "https://www.dnd5eapi.co/api/races"
+        response = get(url)
+        # choose a random race
+        character_races = [
+            response["results"][randint(0, response["count"] - 1)]["index"]
+            for _ in range(NUM_CHARACTERS)
+        ]
+        logging.info(f"Created race: {character_races}")
+    except Exception:
+        logging.info(f"Error retrieving races: {Exception}")
+    try:
+        save_json(character_races, "races")
+    except Exception:
+        logging.info(f"Error saving races: {Exception}")
+
 
 start = DummyOperator(
-    task_id='start',
+    task_id="start",
     dag=first_dag,
 )
 
 create_name = PythonOperator(
-    task_id='create_name',
+    task_id="create_name",
     dag=first_dag,
     python_callable=_generate_name,
 )
 
-create_attributes = DummyOperator(
-    task_id='create_attributes',
+create_attributes = PythonOperator(
+    task_id="create_attributes",
     dag=first_dag,
+    python_callable=_generate_attribute,
 )
 
-create_languages = DummyOperator(
-    task_id='create_languages',
+create_languages = PythonOperator(
+    task_id="create_languages",
     dag=first_dag,
+    python_callable=_generate_language,
 )
 
 create_class = PythonOperator(
-    task_id='create_class',
+    task_id="create_class",
     dag=first_dag,
     python_callable=_generate_class,
 )
 
 create_proficiency = DummyOperator(
-    task_id='profficiency_choices',
+    task_id="profficiency_choices",
     dag=first_dag,
 )
 
-create_race = DummyOperator(
-    task_id='create_race',
+create_race = PythonOperator(
+    task_id="create_race",
     dag=first_dag,
+    python_callable=_generate_race,
 )
 
-create_level = DummyOperator(
-    task_id='create_level',
+create_level = PythonOperator(
+    task_id="create_level",
     dag=first_dag,
+    python_callable=_generate_level,
 )
 
 create_spells = DummyOperator(
-    task_id='create_spells',
+    task_id="create_spells",
     dag=first_dag,
 )
 
 remove_previous_characters = DummyOperator(
-    task_id='remove_previous_characters',
+    task_id="remove_previous_characters",
     dag=first_dag,
 )
 
 add_new_characters = DummyOperator(
-    task_id='add_new_characters',
+    task_id="add_new_characters",
     dag=first_dag,
 )
 end = DummyOperator(
-    task_id='end',
+    task_id="end",
     dag=first_dag,
-    trigger_rule='none_failed',
+    trigger_rule="none_failed",
 )
 
-creation_tasks =  [create_name, create_attributes, create_languages, create_class, create_proficiency, create_race, create_level, create_spells]
+creation_tasks = [
+    create_name,
+    create_attributes,
+    create_languages,
+    create_class,
+    create_proficiency,
+    create_race,
+    create_level,
+]
 start >> creation_tasks
-creation_tasks >> remove_previous_characters
+creation_tasks >> create_spells
+create_spells >> remove_previous_characters
 remove_previous_characters >> add_new_characters
 add_new_characters >> end
--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,9 @@
 import urllib.parse
 import urllib.request
 import json
+import faker
+from random import randint # for getting random integers, choosing a random item in a response
+import logging
 
 
 url = 'https://www.dnd5eapi.co/api/classes'
@@ -15,3 +18,27 @@ def get(url):
     
     # parse json in python dict
     return json.loads(raw_data.decode(encoding))
+def _generate_class():
+    # get the list of classes from the api
+    try:
+        url = 'https://www.dnd5eapi.co/api/classes'
+        response = get(url)
+        # choose a random class from there
+        character_classes = [response["results"][randint(0, response["count"])]["index"] for _ in range(5)]
+        logging.info(f'Created class: {character_classes}')
+    except Exception:
+        logging.info(f"Error retrieving classes: {Exception.args}")
+    try:
+        save_json(character_classes, "classes")
+    except Exception:
+        logging.info(f"Error saving classes: {Exception.args}")
+
+def save_json(data, file_name):
+    with open(f'dags/temp/{file_name}.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+def json_test_read():
+    with open('dags/temp/names.json') as json_file:
+        data = json.load(json_file)
+        for d in data: print(f"{d}\n")
+# get(url)
